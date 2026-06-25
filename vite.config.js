af43ddce5cba3739194e33683a4ba65dc9c16bc5
WAHIDI1970 <mouad066989gmail.com>
2026-06-25 18:30:14 +0200

    prod

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Remplacez <nom-de-votre-depot> par le nom exact de votre dépôt GitHub (ex: 'nick')
+  base: '/NICK/', 
 })
